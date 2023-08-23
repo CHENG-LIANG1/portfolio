e8fe01d0b29cacfce83dd89429a0008307f1358a
Cheng <madfoxfoxy@gmail.com>
2023-08-24 01:48:37 +0800

    update

--- src/components/util/YouTubeEmbed.jsx
+++ src/components/util/YouTubeEmbed.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-
-const YoutubeEmbed = ({ videoUrl }) => (
+import './styles.css'
+const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
       width="853"
       height="480"
-      src={videoUrl}
+      src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
